9f5dd5226528b7311a79a0eb511816595956cf90
Michal Skalski <micha.skalski@gmail.com>
2016-05-28 14:58:26 +0200

    Initial Commit

---
+++ CrossAndCircle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrossAndCircle
+//
+//  Created by Michał Skalski on 28.05.2016.
+//  Copyright © 2016 Michał Skalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
